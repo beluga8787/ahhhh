b17d412976c68691ebc88d15bfaac6cea35d293b
Gitler <GitlerZZZoMaxoPutin@company.com>
2026-06-03 18:37:31 +0300

    ss

---
+++ Desktop/вапвап/src/compot/ProductCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+    name: String,
+    price: Number,
+    Instock: Number
+})
+
+</script>
+
+<template>
+    <h2>{{ name }}</h2>
+    <p>${{ price }}</p>
+    <p>В а нале: {{ Instock }}</p>
+
+    <button>buy</button>
+</template>
+
+
+
+<style>
+button {
+  border-radius: 4px;
+}
+</style>
